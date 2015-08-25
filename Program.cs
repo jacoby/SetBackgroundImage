65c9f8968223108f8c322ebf7a480bc7564a0f0c
Dave Jacoby <jacoby.david@gmail.com>
2015-08-25 14:58:15 -0400

    The Code What Does It

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetBackgroundImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Must Enter an Image");
+                return;
+            }
+            string file = args[0];
+            Console.WriteLine(file);
+            if (!File.Exists(file))
+            {
+                Console.WriteLine("File Doesn't Exist: " + file);
+                return;
+            }
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            key.SetValue(@"WallpaperStyle", "10");
+            key.SetValue(@"TileWallpaper", "0");
+            key.Close();
+            string ext = Path.GetExtension(file);
+            Console.WriteLine(ext);
+            if (!ext.Equals(".jpg", StringComparison.OrdinalIgnoreCase) && !ext.Equals(".jpeg", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("File isn't JPG: " + file);
+                return;
+            }
+            if (!SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, file, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE))
+            {
+                throw new Win32Exception();
+            }
+        }
+
+        private const uint SPI_SETDESKWALLPAPER = 20;
+        private const uint SPIF_UPDATEINIFILE = 0x01;
+        private const uint SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        private static extern bool SystemParametersInfo(uint uiAction, uint uiParam,
+            string pvParam, uint fWinIni);
+    }
+}
